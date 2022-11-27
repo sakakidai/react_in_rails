01e64d3bb97f111f0d317b485c3fcae05a7ca1fa
sakakidai <sd.lybd1016@gmail.com>
2022-11-27 18:06:44 +0900

    イベントのAPI作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root to: 'site#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :events, only: %i[index show create update destroy]
+    end
+  end
 end
